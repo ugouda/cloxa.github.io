e2c0bc44f2d98c3f9bd9bff39808f490e188d0b8
Ujwal Gouda <97618553+ugouda@users.noreply.github.com>
2024-03-24 11:55:02 +0530

    index.js

--- index.js
+++ index.js
@@ -7,34 +7,24 @@ let mon = document.getElementById("mon");
 let ye = document.getElementById("ye");
 let we = document.getElementById("we");
 
-
 setInterval(() => {
-    
     let a = new Date;
     let h = a.getHours();
     let m = a.getMinutes();
     let s = a.getSeconds();
-    // let milisec = a.getMilliseconds();
     let d = a.getDate();
     let month = a.getMonth();
-    // console.log(a.getMonth())
     let year = a.getFullYear()
     let week = a.getDay();
-    // console.log(ye)
-    // let dat = d.toString();
-    // console.log(a)
 
-    let weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    const monthname = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    let weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const monthname = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
     hr.innerHTML = String(h).padStart(2, 0);
     min.innerHTML = String(m).padStart(2, 0);
     sec.innerHTML = String(s).padStart(2, 0);
-    // mili.innerHTML = String(milisec).padStart(2, 0);
     da.innerHTML = String(d).padStart(2, 0);
     mon.innerHTML = monthname[month];
     ye.innerHTML = year;
-    we.innerHTML = weekday[week]
-    // da.innerHTML = dat;
+    we.innerHTML = weekday[week];
 }, 1000);
-// console.log(a)
